054fff64e728e5c7873f624cc2fcd66d18d88047
Amanda Suamy <anandasuamy@gmail.com>
2024-01-05 08:23:35 -0300

    att telas

--- src/pages/Carousel/TipoTela.jsx
+++ src/pages/Carousel/TipoTela.jsx
@@ -2,21 +2,28 @@ import React from "react";
 import Carousel from "./carousel";
 import { NavLink } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
+import "./TipoTela.css"
+import Design from "../../imagens/saudacao/Design.svg"
+
 
 export default function TipoTela() {
   return (
     <>
-    <div className="NavBar">
+    <div className="Navbar">
       <Navbar />
     </div>
-
-    <div>
+    <div className="telas">
+    
+    <div className="recepcao">
       <NavLink to="/telas/tvRecepcao" target="blank">
         <h1>Tv Recepção</h1>
       </NavLink>
+      </div>
+      <div className="torre">
       <NavLink to="/telas/tvTorre" target="blank">
         <h1>Tv Torre</h1>
       </NavLink>
+    </div>
     </div>
     </>
   );
